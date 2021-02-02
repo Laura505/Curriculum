8638851edaf31a3a84169692c2cfb6710f05f489
Laura505 <laurapg22@hotmail.com>
2021-02-02 18:33:20 +0100

    curriculum

---
+++ index.js
@@ -0,0 +1,12 @@
+function datospersonales() {
+    alert('Nombre: Laura Perez Gómez');
+  };
+  function formacionacademica() {
+    alert('Formación relacionada con la formación oficial');
+  };
+  function formacioncomplementaria() {
+    alert('Formación relacionada con la formación no oficial');
+  };
+  function experienciaprofesional() {
+    alert('Experiencia en el mercado laboral');
+  };
